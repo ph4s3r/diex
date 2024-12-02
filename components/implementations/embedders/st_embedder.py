5a92708802cd4c94d2b3d043fa037037c63611fe
ph4s3r <peterkaracsonyi85@gmail.com>
2024-12-02 22:06:33 +0100

    add torch cache flush

--- components/implementations/embedders/st_embedder.py
+++ components/implementations/embedders/st_embedder.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from typing import List
+import torch
 
 from sentence_transformers import SentenceTransformer
 from langchain_core.documents import Document
@@ -26,9 +27,8 @@ class SentenceTransformerEmbedder(Embedder):
         try:
             self.model = SentenceTransformer(self.embedding_model, trust_remote_code=True, device="cuda")
             self.logger.info(f"{self.embedding_model} loaded successfully.")
-            # self.logger.info("Max Sequence Length:", self.model.max_seq_length, "changing it to ")
+            self.logger.info("Max Sequence Length:", self.model.max_seq_length, "changing it to 512")
             self.model.max_seq_length = 512
-            # self.model.tokenizer.padding_side = "right"
 
         except Exception as e:
             self.logger.error(f"Failed to load {self.embedding_model} model: {e}")
@@ -40,7 +40,7 @@ class SentenceTransformerEmbedder(Embedder):
             self.logger.error("No documents provided for embedding.")
             os._exit(199)
 
-        batch_size = 2500
+        batch_size = 6000
         embedding_vectors = list()
 
         batches = (len(documents) // batch_size) + 1
@@ -51,32 +51,14 @@ class SentenceTransformerEmbedder(Embedder):
 
             dox = [doc.page_content for doc in documents[range_start:range_end]]
             
-            embeddings_nparray = self.model.encode(dox)
-            self.logger.info(f"docs embedded successfully, embeddings type: {type(embeddings_nparray)} shape: {embeddings_nparray.shape}")
+            embeddings_nparray = self.model.encode(dox, batch_size=16)
+            
             embedding_vectors.extend(embeddings_nparray.tolist())
-
+            del embeddings_nparray
+            del dox
+            torch.cuda.empty_cache()
+        self.logger.info(f"docs embedded successfully, embeddings type: {type(embedding_vectors)} shape: {embedding_vectors.shape}")
         return embedding_vectors
 
-
-
-        # code for multi-cpu or multi-gpu training:
-
-        # Important, you need to shield your code with if __name__. Otherwise, CUDA runs into issues when spawning new processes.
-        # if __name__ == "__main__":
-        #     # Create a large list of 100k sentences
-        #     sentences = [f"This is sentence {i}" for i in range(100000)]
-
-        #     # Define the model
-        #     model = SentenceTransformer("all-MiniLM-L6-v2")
-
-        #     # Start the multi-process pool on all available CUDA devices
-        #     pool = model.start_multi_process_pool()
-
-        #     # Compute the embeddings using the multi-process pool
-        #     emb = model.encode_multi_process(sentences, pool)
-        #     print("Embeddings computed. Shape:", emb.shape)
-
-        #     # Optional: Stop the processes in the pool
-        #     model.stop_multi_process_pool(pool)
 
 
