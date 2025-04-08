e89aedeaf46ddcbd5e9b86be0cb8a2dc84844e7c
ph4s3r <spec1al@vipmail.hu>
2025-04-08 17:12:22 +0200

    add & set voyagesplitter & add new stella api embedder config

--- container.py
+++ container.py
@@ -1,9 +1,10 @@
 from dependency_injector import containers, providers
 from components.implementations.document_loaders.Unstructuredmdloader import UnstructuredMDLoader
 from components.implementations.document_loaders.pdf_loader import PDFLoader
-from components.implementations.document_splitters.TransformersSplitter import TransformersSplitter
+# from components.implementations.document_splitters.TransformersSplitter import TransformersSplitter
+from components.implementations.document_splitters.VoyageSplitter import VoyageSplitter
 from components.implementations.embedders.api_embedder import GeneralEmbeddingAPIClient
-from components.implementations.embedders.st_embedder import SentenceTransformerEmbedder
+# from components.implementations.embedders.st_embedder import SentenceTransformerEmbedder
 from components.implementations.vectordb_inserters.pinecone_upserter import PineConeUpserter
 from components.services.vector_indexer import VectorIndexer
 
@@ -30,7 +31,7 @@ class Container(containers.DeclarativeContainer):
     )
     
     document_splitter = providers.Singleton(
-        TransformersSplitter,
+        VoyageSplitter,
         max_token_seq_len=config.splitter.tokensplitter.max_token_seq_len,
         token_overlap=config.splitter.tokensplitter.token_overlap
     )
