9c85bd54b28f358fa5c9d090f8b2fe207d690562
ph4s3r <peterkaracsonyi85@gmail.com>
2025-04-19 21:26:38 +0200

    adjust tests to built-in dummy url

--- tests/pdf_test.py
+++ tests/pdf_test.py
@@ -13,31 +13,28 @@ API_URL = "http://localhost:5010/api/parseDocument?renderFormat=all"
 def test_conntest_with_invalid_api_url():
     """Test connection checker method."""
     api_url = "/api/parseDocument?renderFormat=all"
-    test_url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
 
     loader = PDFLoader(file_path=PDF_TEST_FOLDER, api_url=api_url, api_url_ocr=None)
 
     with pytest.raises(SystemExit):
-        loader.conntest(filepath_or_url=test_url, sleep_time=1)
+        loader.conntest(sleep_time=1)
 
 
 def test_conntest_with_incorrect_api_url():
     """Test connection checker method."""
     api_url = "http://localhost:9010/api/parseDocument?renderFormat=all"
-    test_url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
 
     loader = PDFLoader(file_path=PDF_TEST_FOLDER, api_url=api_url, api_url_ocr=None)
 
     with pytest.raises(SystemExit):
-        loader.conntest(filepath_or_url=test_url, sleep_time=1)
+        loader.conntest(sleep_time=1)
 
 
 def test_conntest():
     """Test connection checker method."""
-    test_url = "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
 
     loader = PDFLoader(file_path=PDF_TEST_FOLDER, api_url=API_URL, api_url_ocr=None)
-    assert loader.conntest(filepath_or_url=test_url) is None
+    assert loader.conntest() is None
 
 
 def test_with_vector_pdf():
