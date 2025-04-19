8395f13e39c70ec6973d367e29ed3f4442f4957b
balintrakonczai <rakonczaibalint@gmail.com>
2025-04-19 16:29:21 +0200

    Refactor PDF parser class, to be able to parse a single file only Adding raster PDF test, the implementation seems not working

--- tests/pdf_test.py
+++ tests/pdf_test.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 
 import pytest
+from llmsherpa.readers import Document
 
 from components.implementations.document_loaders.LLMSherpaPDFLoader import PDFLoader
 
@@ -41,6 +42,24 @@ def test_conntest():
 
 def test_with_vector_pdf():
     """Test parsing a single page vector PDF."""
+    test_pdf = PDF_TEST_FOLDER / "LHH_CH_SalaryGuide_2024.pdf"
+    assert test_pdf.exists()
+
     loader = PDFLoader(file_path=PDF_TEST_FOLDER, api_url=API_URL, api_url_ocr=None)
-    docs = loader.load()
-    assert docs
+    sherpa_doc = loader.process_pdf(test_pdf)
+    assert isinstance(sherpa_doc, Document)
+    assert sherpa_doc.to_text(), "Error parsing PDF, content is expected"
+    # Do other content related tests if it would have benefit
+
+
+def test_with_raster_pdf():
+    """Test parsing a single page raster PDF."""
+    test_pdf = PDF_TEST_FOLDER / "LHH_CH_SalaryGuide_2024_raster.pdf"
+    assert test_pdf.exists()
+    api_url_ocr = "http://localhost:5010/api/parseDocument?renderFormat=all&applyOcr=yes"
+
+    loader = PDFLoader(file_path=PDF_TEST_FOLDER, api_url=API_URL, api_url_ocr=api_url_ocr)
+    sherpa_doc = loader.process_pdf(test_pdf)
+    assert isinstance(sherpa_doc, Document)
+    # FYI: it does not work as expected, the implementation need to be fixed
+    assert sherpa_doc.to_text(), "Error parsing PDF, content is expected"
