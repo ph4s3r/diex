684f9fcab949f38a98e817831f1cfe4a7d79eeb7
ph4s3r <peterkaracsonyi85@gmail.com>
2025-04-20 14:33:46 +0200

    move debug chunk json saver

--- components/implementations/semantic_chunkers/UnstructuredChunkers.py
+++ components/implementations/semantic_chunkers/UnstructuredChunkers.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 from typing import List
 from langchain_core.documents import Document
@@ -15,6 +16,16 @@ class UnstructuredHTMLChunker(SemanticChunker):
 
     def chunk(self, documents: List[Document]) -> List[Document]:
 
+        docs = documents
+
+        if self.debug:
+            saveout = [
+                {"content-length": len(d.page_content), "content": d.page_content}
+                for d in docs
+            ]
+            with open("parsed-pdf-chunks.json", "w", encoding="utf-8") as f:
+                json.dump(saveout, f, indent=2, ensure_ascii=False)
+
         return documents
     
 
@@ -22,9 +33,20 @@ class UnstructuredMarkdownChunker(SemanticChunker):
 
     def __init__(self) -> None:
         
+        self.debug = False
         self.logger: logging.Logger = logging.getLogger('SemanticChunker')
 
 
     def chunk(self, documents: List[Document]) -> List[Document]:
+
+        docs = documents
+
+        if self.debug:
+            saveout = [
+                {"content-length": len(d.page_content), "content": d.page_content}
+                for d in docs
+            ]
+            with open("parsed-md-chunks.json", "w", encoding="utf-8") as f:
+                json.dump(saveout, f, indent=2, ensure_ascii=False)
 
         return documents
